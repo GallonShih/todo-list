b8fa170c2b5e4508a7e0a08b7f3cd77853e90c94
GallonShih <Gallon.Shih@testritegroup.com>
2022-01-19 13:14:52 +0800

    refactor: modify mongoose config

--- app.js
+++ app.js
@@ -1,40 +1,19 @@
 // 載入 express 並建構應用程式伺服器
 const express = require('express')
-const mongoose = require('mongoose') // 載入 mongoose
 const exphbs = require('express-handlebars')
-// 引用 body-parser
 const bodyParser = require('body-parser')
-// 載入 method-override
 const methodOverride = require('method-override')
 
-const Todo = require('./models/todo') // 載入 Todo model
-
-// 引用路由器
 const routes = require('./routes')
 const app = express()
 
-mongoose.connect('mongodb://localhost/todo-list', { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
-
-// 取得資料庫連線狀態
-const db = mongoose.connection
-// 連線異常
-db.on('error', () => {
-  console.log('mongodb error!')
-})
-// 連線成功
-db.once('open', () => {
-  console.log('mongodb connected!')
-})
+require('./config/mongoose')
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
-// 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
-// 設定每一筆請求都會透過 methodOverride 進行前置處理
 app.use(methodOverride('_method'))
 
-// 設定首頁路由
-// 將 request 導入路由器
 app.use(routes)
 
 // 設定 port 3000
