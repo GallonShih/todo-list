226b61aa4b0baeb9ffad6d2e643c5a32b33ea9af
GallonShih <Gallon.Shih@testritegroup.com>
2022-03-11 17:18:46 +0800

    feat: add user login feature via passport

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
 const routes = require('./routes')
+const usePassport = require('./config/passport')
 const app = express()
 const PORT = process.env.PORT || 3000
 
@@ -21,6 +22,8 @@ app.use(session({
 }))
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
+
+usePassport(app)
 
 app.use(routes)
 
