289958b6e2f9aded070312b8ce0c5c094f554196
GallonShih <Gallon.Shih@testritegroup.com>
2022-03-14 12:33:32 +0800

    feat: add message partials v2

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ app.use((req, res, next) => {
   res.locals.user = req.user
   res.locals.success_msg = req.flash('success_msg')  // 設定 success_msg 訊息
   res.locals.warning_msg = req.flash('warning_msg')  // 設定 warning_msg 訊息
+  res.locals.error_msg = req.flash('error_msg')  // 設定 warning_msg 訊息
   next()
 })
 app.use(routes)
