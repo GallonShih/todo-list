c82aaeb993a6f75f5561fad86aff20c75e99ea7c
GallonShih <Gallon.Shih@testritegroup.com>
2022-04-01 15:56:47 +0800

    feet: add flash if registered

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -37,7 +37,7 @@ router.post('/register', (req, res) => {
   User.findOne({ email }).then(user => {
     // 如果已經註冊：退回原本畫面
     if (user) {
-      console.log('User already exists.')
+      errors.push({ message: '這個 Email 已經註冊過了。' })
       return res.render('register', {
         errors,
         name,
